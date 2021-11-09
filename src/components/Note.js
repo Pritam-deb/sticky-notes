e4b94a940576347cccb16c82b0d8d2abe215c526
Pritam <pritamdebnath297@gmail.com>
2021-11-10 01:21:44 +0530

    feat: deleting an existing note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
-const Note = ({id, text, date}) => {
+const Note = ({id, text, date, handleDeleteNote}) => {
+
+    const handleClick = () => {
+        handleDeleteNote(id)
+    }
+
     return (
         <div className="note">
             <span>{text}</span>
             <div className="note-footer">
                 <small>{date}</small>
-                <DeleteForeverRoundedIcon className="delete-icon" size="1.3em"/>
+                <DeleteForeverRoundedIcon className="delete-icon" size="1.3em" onClick={handleClick}/>
             </div>
         </div>
     )
